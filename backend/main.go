e0d7752305efad4e75f860f4da8456a96d6a10e0
Ubuntu <ubuntu@ip-172-31-34-93.us-west-2.compute.internal>
2026-05-17 11:26:13 +0000

    add metrics and mod docker-compose

--- backend/main.go
+++ backend/main.go
@@ -3,16 +3,25 @@ package main
 import (
 	"log"
 	"os"
-
+	
 	"github.com/gin-gonic/gin"
 	"github.com/trainwithshubham/skillpulse/database"
 	"github.com/trainwithshubham/skillpulse/handlers"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"go.opentelemetry.io/contrib/instrumentation/github.com/gin-gonic/gin/otelgin"
+
 )
 
 func main() {
+	os.Setenv("OTEL_EXPORTER_OTLP_ENDPOINT", "otel-collector:4317")
 	database.Connect()
 
 	router := gin.Default()
+        
+	// Metrics endpoint
+	router.GET("/metrics", gin.WrapH(promhttp.Handler()))
+
+	router.Use(otelgin.Middleware("skillpulse-backend"))
 
 	// API routes
 	api := router.Group("/api")
